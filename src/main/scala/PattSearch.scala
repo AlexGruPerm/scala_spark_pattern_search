a3086086d98402f8ec08d0abacbe14e2379161a6
AlexGruPerm <ugr@bk.ru>
2018-11-06 15:14:32 +0500

    Prepare bars as columns from source. +---+----------+-------+-------+--------+----------+-------+-------+--------+----------+-------+-------+--------+ | rn|1_ts_begin|1_btype| 1_disp|1_log_co|2_ts_begin|2_btype| 2_disp|2_log_co|3_ts_begin|3_btype| 3_disp|3_log_co| +---+----------+-------+-------+--------+----------+-------+-------+--------+----------+-------+-------+--------+

--- src/main/scala/PattSearch.scala
+++ src/main/scala/PattSearch.scala
@@ -50,13 +50,10 @@ object PattSearch extends App {
     .getOrCreate()
   */
 
-  import org.apache.spark.sql.Row
-  import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
-  import org.apache.spark.sql.types._
-
   /** UDAF, for accumulate all rows as whole instance for compare laster with pattern.
     *
     */
+/*
   class ComparePatter() extends UserDefinedAggregateFunction {
 
     // Input Data Type Schema of Rows.
@@ -69,44 +66,56 @@ object PattSearch extends App {
                                             )
 
     // Intermediate Schema
-    def bufferSchema = StructType(Array(
-      StructField("sum", DoubleType),
-      StructField("cnt", LongType)
+    def bufferSchema  = StructType(Array(
+      StructField("ts_begin", IntegerType),
+      StructField("btype",    StringType),
+      StructField("disp",     DoubleType),
+      StructField("log_co",   DoubleType)
     ))
 
-    // Returned Data Type .
-    def dataType: DataType = DoubleType
+    // Returned Data Type.
+    def dataType: DataType =
+      StructType(Array(
+                StructField("ts_begin", IntegerType),
+                StructField("btype",    StringType),
+                StructField("disp",     DoubleType),
+                StructField("log_co",   DoubleType)
+    ))
 
     // Self-explaining
     def deterministic = true
 
     // This function is called whenever key changes
     def initialize(buffer: MutableAggregationBuffer) = {
-      buffer(0) = 0.toDouble // set sum to zero
-      buffer(1) = 0L // set number of items to 0
+      buffer(0, new java.util.ArrayList[Array[
+                                                         StructField("ts_begin", IntegerType),
+                                                         StructField("btype",    StringType),
+                                                         StructField("disp",     DoubleType),
+                                                         StructField("log_co",   DoubleType)
+                                                       ]])
+      //  buffer(0) = 0.toDouble // set sum to zero
+      //  buffer(1) = 0L // set number of items to 0
     }
 
     // Iterate over each entry of a group
     def update(buffer: MutableAggregationBuffer, input: Row) = {
-      // With [0] - java.lang.ClassCastException: java.lang.Integer cannot be cast to java.lang.Double
-      // Because 0 it's a ts_begin (Int)
-      buffer(0) = buffer.getDouble(0) + input.getDouble(2)
-      buffer(1) = buffer.getLong(1) + 1
+      // java.lang.IllegalArgumentException: Could not update 4th value in this buffer because it only has 4 values.
+      buffer(buffer.size+1) = input.getInt(0)
+      otocLogg.log.info("buffer.size="+buffer.size+" input="+input)
     }
 
     // Merge two partial aggregates
     def merge(buffer1: MutableAggregationBuffer, buffer2: Row) = {
-      buffer1(0) = buffer1.getDouble(0) + buffer2.getDouble(2) // get field disp
-      buffer1(1) = buffer1.getLong(1) + 1//buffer2.getLong(1)
     }
 
     // Called after all the entries are exhausted.
     def evaluate(buffer: Row) = {
-      buffer.getDouble(0)/buffer.getLong(1).toDouble
+      //buffer.getDouble(0)/buffer.getLong(1).toDouble
+      buffer.size
     }
 
   }
-
+*/
 
 
 
@@ -137,33 +146,65 @@ object PattSearch extends App {
             thisRow.getAs("log_co").toString)
   }
 
-  //df.withColumn("colD", collect_set("colC").over(Window.partitionBy("colA").orderBy("colB"))).show(false)
 
+
+  otocLogg.log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
   otocLogg.log.info("~ with window show ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-  // Window function example
   import org.apache.spark.sql.expressions.Window
-  import org.apache.spark.sql.functions.col
+  import org.apache.spark.sql.functions.{col, _}
+  import spark.implicits._
 
+  /*
   val windowSpec = Window
     .orderBy(col("ts_begin").asc)
     .rowsBetween(Window.currentRow, 2)
+*/
 
+  val nBuckets=3
+
+  listBars
+    .withColumn("rn", row_number() over Window.orderBy(col("ts_begin").asc) )
+    .withColumn("rn", floor(($"rn"-1)/3))
+    .withColumn("btype",when(col("btype") === lit("g"), 1)
+                                 .when(col("btype") === lit("r"), -1)
+                                 .otherwise(0))
+    .withColumn("rnk", row_number() over Window.partitionBy("rn").orderBy(col("ts_begin").asc) )
+    //-------------------------
+    .groupBy("rn")
+    .pivot("rnk", 1 to nBuckets)
+    .agg(
+      sum("ts_begin").alias("ts_begin"),
+      sum("btype").alias("btype"),
+      sum("disp").alias("disp"),
+      sum("log_co").alias("log_co")
+    )
+    //-------------------------
+    .show
+
+  /*
+   .pivot("date") \
+    .agg(
+       avg('cost').alias('cost'),
+       first("ship").alias('ship')
+    )
+  */
+
+
+  // df.withColumn("ration", col("count1").divide(col("count"))
+
+  /*
   val compPatt = new ComparePatter()
-//df.groupBy("group_id").agg(expr("gm(id) as GeometricMean")).show()
+
   listBars.withColumn("comp_patt", compPatt(listBars.col("ts_begin"),
                                                       listBars.col("btype"),
                                                       listBars.col("disp"),
                                                       listBars.col("log_co")
                                                      ) over windowSpec).show()
-
+  */
   otocLogg.log.info("~ with window show ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+  otocLogg.log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-  /*
-  listBars.createOrReplaceTempView("t_last_bars")
-  otocLogg.log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-  spark.sql("SELECT * FROM t_last_bars").show()
-  otocLogg.log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
- */
+
 
   val t2_common = System.currentTimeMillis
   otocLogg.log.info("================== SUMMARY ========================================")
